1732921556c032e6eb88af822c59afa856627fa2
Karthiga Karthikeyan <karthiga.k1989@gmail.com>
2022-01-28 19:30:48 +0530

    FirstCommit

---
+++ src/main/java/com/stacifysimple/springrestservices/SpringbootrestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.stacifysimple.springrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacifysimple/springrestservices/SpringbootrestservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacifysimple.springrestservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrestservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
