5a8904285052d15e7fd03859d8eaa0097d848399
Karthiga Karthikeyan <karthiga.k1989@gmail.com>
2022-02-01 13:56:07 +0530

    Second Commit --adding comment line

--- src/main/java/com/stacifysimple/springrestservices/SpringbootrestservicesApplication.java
+++ src/main/java/com/stacifysimple/springrestservices/SpringbootrestservicesApplication.java
@@ -2,7 +2,7 @@ package com.stacifysimple.springrestservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*Spring Boot starter Project*/
 @SpringBootApplication
 public class SpringbootrestservicesApplication {
 
